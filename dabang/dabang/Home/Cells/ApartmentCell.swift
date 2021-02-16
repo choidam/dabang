425673bfefe9165da49f3b7a6b495104e9a43744
dam <37874238+choidam@users.noreply.github.com>
2021-02-16 21:39:45 +0900

    update: 아파트, 오피스텔 UI 구현

--- dabang/dabang/Home/Cells/ApartmentCell.swift
+++ dabang/dabang/Home/Cells/ApartmentCell.swift
@@ -11,6 +11,31 @@ import Then
 
 class ApartmentCell: UITableViewCell {
     static let identifier = "apartmentCell"
+    var divideView = UIView().then {
+        $0.backgroundColor = .verylightGray
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    var roomImageView = UIImageView().then {
+        $0.backgroundColor = .lightGray
+        $0.layer.cornerRadius = 4
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    var checkImageView = UIImageView().then {
+        $0.image = UIImage(named: "star_fill")
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    var priceLabel = UILabel().then {
+        $0.text = "월세 1000/50"
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    var roomTypeLabel = UILabel().then {
+        $0.text = "아파트"
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+    var descLabel = UILabel().then {
+        $0.text = "여의도 역세권 가격도 저렴한 도보 10분 안전 보장~~~!"
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
@@ -23,6 +48,43 @@ class ApartmentCell: UITableViewCell {
 // MARK: - UISetup
 extension ApartmentCell {
     private func setupUI(){
-        contentView.backgroundColor = .yellow
+        addSubview(divideView)
+        divideView.snp.makeConstraints{
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(113)
+            $0.height.equalTo(1)
+        }
+        addSubview(roomImageView)
+        roomImageView.snp.makeConstraints{
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(13)
+            $0.width.equalTo(126)
+            $0.height.equalTo(84)
+        }
+        addSubview(checkImageView)
+        checkImageView.snp.makeConstraints{
+            $0.leading.equalTo(17)
+            $0.top.equalTo(18)
+            $0.width.height.equalTo(18)
+        }
+        addSubview(priceLabel)
+        priceLabel.setPriceLabel()
+        priceLabel.snp.makeConstraints{
+            $0.leading.equalTo(roomImageView.snp.trailing).offset(34)
+            $0.top.equalTo(18)
+        }
+        addSubview(roomTypeLabel)
+        roomTypeLabel.setRoomTypeLabel()
+        roomTypeLabel.snp.makeConstraints{
+            $0.leading.equalTo(roomImageView.snp.trailing).offset(34)
+            $0.top.equalTo(priceLabel.snp.bottom).offset(4)
+        }
+        addSubview(descLabel)
+        descLabel.setDescriptionLable()
+        descLabel.snp.makeConstraints{
+            $0.leading.equalTo(roomImageView.snp.trailing).offset(34)
+            $0.trailing.equalTo(-24)
+            $0.top.equalTo(roomTypeLabel.snp.bottom)
+        }
     }
 }
