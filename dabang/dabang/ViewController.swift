8d48fd81b7ad8132550b6413e2ca39bd977aa686
dam <37874238+choidam@users.noreply.github.com>
2021-02-16 14:39:11 +0900

    create: 프로젝트 생성

---
+++ dabang/dabang/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  dabang
+//
+//  Created by 최담 on 2021/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
