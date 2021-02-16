6ac0488831435d21954125181d94ace58e06fbfd
dam <37874238+choidam@users.noreply.github.com>
2021-02-16 14:43:41 +0900

    chore: swiftlint 적용

--- dabang/dabang/ViewController.swift
+++ dabang/dabang/ViewController.swift
@@ -11,11 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
 
-
 }
-
